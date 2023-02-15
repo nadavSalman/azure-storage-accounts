0bcaf9adcd4a307701cd201983e51558cf3b2579
nsalman <nsalman@attentigroup.com>
2023-02-15 15:43:07 +0200

    Start blob prositioninig with python client

--- provision-storage-account.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-az storage account list --resource-group NadavHomework --query '[*].name' --output tsv | while read -r storage_account_name; do
-  printf "Storage Account Name : %s \n" $storage_account_name
-done
-
